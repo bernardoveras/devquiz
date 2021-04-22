1dba760734d2805844b122d94a106a5a42e26bd3
Bernardo Veras <bernardo@vvssistemas.com.br>
2021-04-22 00:01:41 -0300

    Separado o componente para app bar

--- lib/shared/constants/app_text_styles.dart
+++ lib/shared/constants/app_text_styles.dart
@@ -36,7 +36,7 @@ class AppTextStyles {
 
   static final TextStyle body = GoogleFonts.notoSans(
     color: AppColors.grey,
-    fontSize: 13.sp,
+    fontSize: 15.sp,
     fontWeight: FontWeight.normal,
   );
 
