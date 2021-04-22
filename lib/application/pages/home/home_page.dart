f4a1277bc1d6bae68286490db94f5ce7ba3db692
Bernardo Veras <bernardo@vvssistemas.com.br>
2021-04-22 00:03:52 -0300

    Adicionado o padding

--- lib/application/pages/home/home_page.dart
+++ lib/application/pages/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:devquiz/application/pages/home/components/app_bar.dart';
 import 'package:flutter/material.dart';
+import '../../../shared/extensions/screen_util_extension.dart';
 
 class HomePage extends StatelessWidget {
   final double porcentagem = 75;
@@ -8,6 +9,20 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: HomeAppBar(context, porcentagem),
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Padding(
+          padding: EdgeInsets.only(
+            left: 20.width,
+            right: 20.width,
+            top: 120.height,
+            bottom: MediaQuery.of(context).padding.bottom,
+          ),
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
     );
   }
 }
